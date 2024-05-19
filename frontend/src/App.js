66502552d6f19104e70c77c6f7481d30b16e5f43
Brandaddy99 <Brandodangelo99@gmail.com>
2024-05-19 13:28:22 -0700

    I hate the month of May. PS here is the NAV bar!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NavBar from './componets/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          AHHH I HATE MAY but welcome to the website.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
