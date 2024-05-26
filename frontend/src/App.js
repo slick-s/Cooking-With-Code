bc51d1af616628f86e4dd1bf9b90cefd97f3adeb
kayla tapiador <kaylatapiador@yahoo.com>
2024-05-26 12:14:53 -0700

    updated file using axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import NavBar from './componets/NavBar'; // Adjust the import according to your project structure
-import Background from './componets/background'; // Adjust the import according to your project structure
+import NavBar from './components/NavBar'; // Adjust the import according to your project structure
+import Background from './components/background'; // Adjust the import according to your project structure
+import axios from 'axios';
 
 function App() {
   const [recipes, setRecipes] = useState([]); // Initialize state with an empty string
 
   useEffect(() => {
 
-    const fetchUsingPromiseWithFetchApi =()=>{
-        fetch('http://localhost5000/api')
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error('Network response was not ok');
+    const fetchRecipes = async()=>{
+        fetch('http://localhost:5000/api')
+        try{
+          const response = await axios.get('http://localhost:5000/api');
+          if(Array.isArray(response.data)){
+            setRecipes(response.data);
+          }else{
+            throw new Error('Data is not an array');
           }
-          return res.text();
-        }).then((data) => {
-          if(Array.isArray(data)){
-            setRecipes(data);
-          }
-          else{
-            throw new Error('data is not an array');
-          }
-        })
-        .catch((error) => console.error('There was a problem with the fetch operation:', error));
+        }
+        catch(error) {console.error('There was a problem with the fetch operation:', error);} 
 
-        fetchUsingPromiseWithFetchApi();//Trigger fetching method on component mount
+        fetchRecipes();//Trigger fetching method on component mount
       }
     }, []);//Run the effect only once on component mount
     
-      
+   const addRecipe = async(recipe) =>{
+    try{
+      const response = await axios.post('http://localhost5000/recipes',recipe);
+      console.log('Recipe has been added', response.data);
+    }catch(error){
+      console.error('Error with the Post operation', error);
+    }
+   }   
 
   return (
     <div className="App">
@@ -36,12 +39,14 @@ function App() {
       <Background />
       <div className="message-container"> {/* Add a descriptive class name */}
         <h1>Recipes</h1>
-        {recipes.map((recipe)=> (
-          <div className="recipe" key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.body}</p>
-            </div>
-        ))}
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe.id}>
+              <h2>{recipe.title}</h2>
+              <p>{recipe.body}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
