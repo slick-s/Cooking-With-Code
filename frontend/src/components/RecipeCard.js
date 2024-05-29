9f56e7428468dbc1f5e484c7914d50d9fa31a254
TheRealSlick <wadams0107@gmail.com>
2024-05-28 18:40:04 -0700

    finalizing cusines

--- frontend/src/components/RecipeCard.js
+++ frontend/src/components/RecipeCard.js
@@ -3,36 +3,29 @@ import axios, * as others from 'axios';
 
 // React Modules
 import React, { useEffect, useState } from 'react';
-import { ReactDOM } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Card, Button } from 'react-bootstrap';
 
 
-function RecipeCard({country}) {
+function RecipeCard({ country }) {
 
   const [recipes_, setRecipes] = useState([])
   const [description, setDecription] = useState([])
-  
+
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`);
+        const initStartingRecipes = response.data.meals // Array of Meal Objects
+        for (let index = 0; index < initStartingRecipes.length; index++) {
+          const desc = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${initStartingRecipes[index].idMeal}`)
+          initStartingRecipes[index].desc = desc.data.meals[0].strInstructions
+        }
         setRecipes(response.data.meals);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
-    const desc = async () => {
-      try {
-        const response = await axios.get(`www.themealdb.com/api/json/v1/1/lookup.php?i=${description}`);
-        setDecription(response.data.meals);
-      } catch (error) {
-        console.error(error)
-      }
-    }
-
-    desc()
     fetchRecipes();
   }, []);
 
@@ -41,11 +34,10 @@ function RecipeCard({country}) {
       {recipes_.map((recipe => {
         return (
           <Card key={recipe.idMeal} style={{ width: '18rem' }}>
-            <Card.Img src={recipe.strMealThumb} variant="top"  />
+            <Card.Img src={recipe.strMealThumb} variant="top" />
             <Card.Body>
-              
               <Card.Title>{recipe.strMeal}</Card.Title>
-              <Card.Text></Card.Text>
+              <Card.Text>{recipe.desc}</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
---
+++ frontend/src/pages/Egyptian.js
@@ -0,0 +1,11 @@
+import RecipeCard from "../components/RecipeCard"
+
+export default function Egyptian() {
+    return (
+        <div>
+            <h1>Egyptian Recipes</h1>
+            <RecipeCard country={'Egyptian'} />
+        </div>
+       
+    )
+}
