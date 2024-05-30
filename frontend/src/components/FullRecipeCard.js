674eb9ec29377845413510c5a32dcd331e1ad524
kayla tapiador <kaylatapiador@yahoo.com>
2024-05-29 23:46:44 -0700

    FullRecipeCard functional just need some styling updates

--- frontend/src/components/FullRecipeCard.js
+++ frontend/src/components/FullRecipeCard.js
@@ -5,37 +5,51 @@ import React, { useEffect, useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 
-function FullRecipeCard({food}) {
-
+function FullRecipeCard({ food }) {
   const [recipe, setRecipe] = useState(null);
 
   useEffect(() => {
-    const fetchRecipes = async () => {
+    const fetchRecipe = async () => {
       try {
-        const response = await axios.get(`http://www.themealdb.com/api/json/v1/1/lookup.php?i=${food}`);
-       setRecipe(response.data.meals[0]);
-       console.log(setRecipe(response.data.meals[0]))
-
+        const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${food}`);
+        setRecipe(response.data.meals[0]);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
-    fetchRecipes();
+    fetchRecipe();
   }, [food]);
+
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Card>
       <Card.Img src={recipe.strMealThumb} variant="top" />
       <Card.Body>
         <Card.Title>{recipe.strMeal}</Card.Title>
-        
+        <Card.Text>
+          <strong>Category:</strong> {recipe.strCategory}<br />
+          <strong>Cuisine:</strong> {recipe.strArea}<br />
+          <strong>Instructions:</strong> {recipe.strInstructions}
+        </Card.Text>
         <h3>Ingredients</h3>
-        
+        <ul>
+          {[...Array(20)].map((_, i) => {
+            const ingredient = recipe[`strIngredient${i + 1}`];
+            const measure = recipe[`strMeasure${i + 1}`];
+            return ingredient ? (
+              <li key={i}>
+                {ingredient} - {measure}
+              </li>
+            ) : null;
+          })}
+        </ul>
       </Card.Body>
     </Card>
-  )
-
+  );
 }
 
 export default FullRecipeCard;
--- frontend/src/components/RecipeCard.js
+++ frontend/src/components/RecipeCard.js
@@ -4,23 +4,18 @@ import axios, * as others from 'axios';
 // React Modules
 import React, { useEffect, useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 
 function RecipeCard({ country }) {
-
-  const [recipes_, setRecipes] = useState([])
-  const [description, setDecription] = useState([])
+  const [recipes, setRecipes] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`);
-        const initStartingRecipes = response.data.meals // Array of Meal Objects
-        /*for (let index = 0; index < initStartingRecipes.length; index++) {
-          const desc = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${initStartingRecipes[index].idMeal}`)
-          initStartingRecipes[index].desc = desc.data.meals[0].strInstructions
-        }*/
         setRecipes(response.data.meals);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -28,24 +23,22 @@ function RecipeCard({ country }) {
     };
 
     fetchRecipes();
-  }, []);
+  }, [country]);
 
   return (
     <div>
-      {recipes_.map((recipe => {
-        return (
-          <Card key={recipe.idMeal} style={{ width: '18rem' }}>
-            <Card.Img src={recipe.strMealThumb} variant="top" />
-            <Card.Body>
-              <Card.Title>{recipe.strMeal}</Card.Title>
-        
-              <Button as={Link} to ={`/fullrecipe/${recipe.idMeal}`}variant="primary">View Recipe</Button>
-            </Card.Body>
-          </Card>
-        )
-      }))}
+      {recipes.map((recipe) => (
+        <Card key={recipe.idMeal} style={{ width: '18rem' }}>
+          <Card.Img src={recipe.strMealThumb} variant="top" />
+          <Card.Body>
+            <Card.Title>{recipe.strMeal}</Card.Title>
+            <Button onClick={() => navigate(`/fullRecipe/${recipe.idMeal}`)} variant="primary">
+              View Recipe
+            </Button>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
-
   );
 }
 
