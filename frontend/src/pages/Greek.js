b48ddefb4a9322450a3cb222d61baa48a6eb287e
Brandaddy99 <Brandodangelo99@gmail.com>
2024-05-30 18:32:21 -0700

    added styling

--- frontend/src/pages/Greek.js
+++ frontend/src/pages/Greek.js
@@ -3,7 +3,7 @@ import RecipeCard from "../components/RecipeCard"
 export default function Greek() {
     return (
         <div>
-           <h1>Greek Recipes</h1>
+           <h1 className="H-1">Greek Recipes</h1>
             <RecipeCard country={'Greek'} />
         </div>
        
